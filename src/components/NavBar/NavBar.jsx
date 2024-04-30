51f5b4280f67f597843207281b97fdb1a8ebc9d4
goberto88 <goberto88@gmail.com>
2024-04-30 14:33:44 -0400

    nueva modificación

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -14,9 +14,9 @@ const NavBar = () => {
       <nav className="flex items-center justify-between flex-wrap bg-gradient-to-b from-zinc-400 p-10">
         <NavLink to={"/"}>
           <img
-            src="../public/LogoSexDiscos/sexDiscoLogo.png"
+            src="/LogoSexDiscos/sexDiscoLogo.png"
             alt="logo"
-            className="w-48 sm:w-48 md:w-64 lg:w-64 xl:w-96"
+            className="w-32 sm:w-48 md:w-52 lg:w-56"
           />
         </NavLink>
 
@@ -26,13 +26,27 @@ const NavBar = () => {
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
-            className="w-6 h-6"
+            className={`w-6 h-6 ${isOpen ? "hidden" : ""}`}
           >
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth={2}
               d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            className={`w-6 h-6 ${isOpen ? "" : "hidden"}`}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M6 18L18 6M6 6l12 12"
             />
           </svg>
         </div>
@@ -44,35 +58,39 @@ const NavBar = () => {
         >
           <div className="text-base items-center lg:flex-grow">
             <NavLink
+              onClick={toggleMenu}
               to={"categoria/vinilos"}
-              className="block font-MiFuente mt-4 lg:inline-block lg:mt-0 text-black-700  active:scale-110 active:font-black m-4 text-xl"
+              className="block font-MiFuente mt-4 lg:inline-block lg:mt-0 text-black-700  active:scale-110 active:font-black mr-5 text-xl"
             >
               Vinilos
             </NavLink>
 
             <NavLink
+              onClick={toggleMenu}
               to={"categoria/cds"}
-              className="block font-MiFuente mt-4 lg:inline-block lg:mt-0 text-black-700  active:scale-110 active:font-black m-4 text-xl"
+              className="block font-MiFuente mt-4 lg:inline-block lg:mt-0 text-black-700  active:scale-110 active:font-black mr-5 text-xl"
             >
               CDs
             </NavLink>
 
             <NavLink
+              onClick={toggleMenu}
               to={"categoria/cassettes"}
-              className="block font-MiFuente mt-4 lg:inline-block lg:mt-0 text-black-700  active:scale-110 active:font-black m-4 text-xl"
+              className="block font-MiFuente mt-4 lg:inline-block lg:mt-0 text-black-700  active:scale-110 active:font-black mr-5 text-xl"
             >
               Cassettes
             </NavLink>
 
             <NavLink
+              onClick={toggleMenu}
               to={"categoria/camisetas"}
-              className="block font-MiFuente mt-4 lg:inline-block lg:mt-0 text-black-700  active:scale-110 active:font-black m-4 text-xl"
+              className="block font-MiFuente mt-4 lg:inline-block lg:mt-0 text-black-700  active:scale-110 active:font-black mr-5 text-xl"
             >
               Camisetas
             </NavLink>
 
-            <NavLink to={"/cart"}>
-            <CartWidget />
+            <NavLink onClick={toggleMenu} to={"/cart"}>
+              <CartWidget />
             </NavLink>
           </div>
         </div>
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Section = ({ greeting, info }) => {
   return (
     <>
-      <section className="flex flex-col md:flex-row items-center justify-center p-5">
+      <section className="flex flex-col md:flex-row items-center justify-center p-10">
         <div className="secc-text md:w-1/2 md:mr-8 text-center md:text-left">
           <h1 className="text-3xl md:text-4xl font-MiFuente">{greeting}</h1>
           <p className="text-base font-MiFuente md:text-lg mt-3 mb-3 md:mt-5">
@@ -18,7 +18,7 @@ const Section = ({ greeting, info }) => {
         </div>
         <img
           className="imagen-seccion w-full md:w-3/5 mt-5 md:mt-0"
-          src="../public/LogoSexDiscos/seccImg-PhotoRoom.png-PhotoRoom.png"
+          src="/LogoSexDiscos/seccImg-PhotoRoom.png-PhotoRoom.png"
           alt="imagen-seccion"
         />
       </section>
